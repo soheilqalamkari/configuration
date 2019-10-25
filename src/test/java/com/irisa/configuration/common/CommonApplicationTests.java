8255d5afe8670735cfe3fa60cc677c3b3c8f283f
soheil <soheilqalamkari76@gmail.com>
2019-10-25 22:00:28 +0330

    first commit

---
+++ src/test/java/com/irisa/configuration/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisa.configuration.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
